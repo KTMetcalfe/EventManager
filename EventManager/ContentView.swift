bda40ff4365a5004a7c952afd96790a1f31e40de
Kian Metcalfe <kian@metcalfe.net>
2022-06-02 10:26:14 -0500

    Basic UI

--- EventManager/ContentView.swift
+++ EventManager/ContentView.swift
@@ -6,11 +6,55 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    @State private var login: String = ""
+    @State private var location: String = ""
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack {
+            Text("Event Manager")
+                .font(.largeTitle)
+            Form {
+                Section {
+                    TextField("Login Code", text: $login)
+                        .font(.title3)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .keyboardType(.decimalPad)
+                        .onReceive(Just(login)) { newValue in
+                                        let filtered = newValue.filter { "0123456789".contains($0) }
+                                        if filtered != newValue {
+                                            self.login = filtered
+                                        }
+                        }
+                    TextField("Location Code", text: $location)
+                        .font(.title3)
+                        .padding()
+                        .multilineTextAlignment(.center)
+                        .keyboardType(.decimalPad)
+                        .onReceive(Just(location)) { newValue in
+                                        let filtered = newValue.filter { "0123456789".contains($0) }
+                                        if filtered != newValue {
+                                            self.location = filtered
+                                        }
+                        }
+                }
+                HStack() {
+                    Spacer()
+                    Button("Login") {
+                        
+                    }
+                    .font(.title3)
+                    Spacer()
+                }
+            }
+            .frame(maxHeight: 300)
+            .cornerRadius(15)
             .padding()
+        }
     }
 }
 
