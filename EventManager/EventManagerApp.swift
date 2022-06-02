0ae95d8c54228a97a0ceb11ee60c0c8cd30f5578
Kian Metcalfe <kian@metcalfe.net>
2022-06-02 09:36:19 -0500

    Initial Commit

---
+++ EventManager/EventManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventManagerApp.swift
+//  EventManager
+//
+//  Created by Kian Metcalfe on 6/2/22.
+//
+
+import SwiftUI
+
+@main
+struct EventManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
