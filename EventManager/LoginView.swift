e6b8eefa49f26e7ef0214b5cdaa2e1015944e2c7
Kian Metcalfe <kian@metcalfe.net>
2022-06-03 15:39:44 -0500

    Added server api

---
+++ EventManager/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  EventManager
+//
+//  Created by Kian Metcalfe on 6/2/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
